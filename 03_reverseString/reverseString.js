f3f19a6cf62d3a15fd791ecd23d552cba06086fe
Lupis_ <doliver575@gmail.com>
2023-08-09 11:18:55 -0400

    Complete javascript exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let strReverse = "";
+    let strTemp = "";
+    for (let i = 0; i < str.length; i++){
+        strTemp = str.charAt(i);
+        strReverse = strTemp.concat('',strReverse);
+    }
+    return strReverse;
 };
 
 // Do not edit below this line
