8f8ab98feb9e772ad2a7d6ccfd34ef091228e0bd
Lupis_ <doliver575@gmail.com>
2023-08-10 10:14:49 -0400

    Complete javascript exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    finalSum = 0;
 
+    if(!Number.isInteger(int1) || !Number.isInteger(int2)){
+        return 'ERROR';
+    }
+    else if (int1 < 0 || int2 < 0){
+        return 'ERROR'
+    }
+    else if (int1 > int2) {
+        let temp = int1;
+        int1 = int2;
+        int2 = temp;
+    }
+
+    for (let i = int1; i <= int2; i++) {
+        finalSum += i;
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
