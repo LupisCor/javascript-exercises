64cea537ea438b7bb043773e0887b3928f140da5
Lupis_ <doliver575@gmail.com>
2023-08-24 11:08:56 -0400

    Complete javascript exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sumTotal = 0;
+  arr.forEach(element => sumTotal+=element);
+  return sumTotal;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = 1;
+  arr.forEach(element => product*=element);
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 || num === 1){
+    return 1;
+  }
+  for (i = num-1; i>= 1; i--){
+    num *= i;
+  }
+  return num;
 };
 
 // Do not edit below this line
