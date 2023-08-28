f1be9c5a224a8d416de4d24774bfa742e8eb5a69
Lupis_ <doliver575@gmail.com>
2023-08-28 11:10:40 -0400

    Complete javascript exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let fib = Number(num);
+    if (fib < 0) {
+        return "OOPS";
+    }
+    if (fib <= 1) {
+        return fib;
+    }
+    return (fibonacci(num - 1) + fibonacci(num -2));
 };
 
 // Do not edit below this line
